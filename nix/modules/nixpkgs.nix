a6d3998daf50e406ba67b6d3f0d1f635b340f7a5
Sridhar Ratnakumar <srid@srid.ca>
2024-04-04 12:09:39 +1100

    we

--- nix/modules/nixpkgs.nix
+++ nix/modules/nixpkgs.nix
@@ -2,7 +2,7 @@
 { inputs, flake-parts-lib, ... }: {
   options.perSystem = flake-parts-lib.mkPerSystemOption ({ pkgs, system, ... }: {
     imports = [
-      "${(flake-parts-lib.findInputByOutPath ./. inputs).nixpkgs}/nixos/modules/misc/nixpkgs.nix"
+      "${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix"
     ];
     nixpkgs.hostPlatform = system;
   });
