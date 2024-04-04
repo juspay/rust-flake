1c2d168fe0d4fc8347762ae8ded3636cf9cd0b2d
Sridhar Ratnakumar <srid@srid.ca>
2024-04-04 12:09:03 +1100

    fix

--- nix/modules/nixpkgs.nix
+++ nix/modules/nixpkgs.nix
@@ -1,6 +1,6 @@
 # https://github.com/hercules-ci/flake-parts/issues/74#issuecomment-1513708722
 { inputs, flake-parts-lib, ... }: {
-  options.perSystem = mkPerSystemOption ({ pkgs, system, ... }: {
+  options.perSystem = flake-parts-lib.mkPerSystemOption ({ pkgs, system, ... }: {
     imports = [
       "${(flake-parts-lib.findInputByOutPath ./. inputs).nixpkgs}/nixos/modules/misc/nixpkgs.nix"
     ];
