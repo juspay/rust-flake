f2333101b401e82013b2c6966a3727bdd23c86e0
shivaraj-bh <sbh69840@gmail.com>
2024-06-18 16:55:23 +0530

    chore: Use latest crane
    https://github.com/ipetkov/crane/issues/527 is now fixed

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    # https://github.com/ipetkov/crane/issues/527
-    crane.url = "github:ipetkov/crane/2c653e4478476a52c6aa3ac0495e4dea7449ea0e"; # Cargo.toml parsing is broken in newer crane (Mar 24)
+    crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { rust-overlay, crane, ... }: {
