b5f39885e2fcf137bfaf75decc077f9cca2bd984
Abhishek Singh <abhishek17513@gmail.com>
2025-01-14 03:46:52 +0530

    rust-overlay: Follow nixpkgs input
    https://github.com/juspay/rust-flake/discussions/29
    
    * fix: remove crane.inputs.nixpkgs.follows
    
    `crane` no longer has `nixpkgs` as an input.
    
    Refs: ipetkov/crane@6f7504a#diff-206b9ce276ab5971a2489d75eb1b12999d4bf3843b7988cbe8d687cfde61dea0
    
    * feat: make rust-overlay follow nixpkgs
    
    * feat: update `rust-overlay` input

--- flake.nix
+++ flake.nix
@@ -3,34 +3,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.flake = false;
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { rust-overlay, crane, ... }:
-  let
-    # Preserve name and key of the module file for dedup and error message locations
-    import' =
-      modulePath: staticArg:
+    let
+      # Preserve name and key of the module file for dedup and error message locations
+      import' =
+        modulePath: staticArg:
         {
           key = toString modulePath;
           _file = toString modulePath;
           imports = [ (import modulePath staticArg) ];
         };
-  in
-  {
-    flakeModules = {
-      default = import' ./nix/modules/flake-module.nix { inherit rust-overlay crane; };
-      nixpkgs = ./nix/modules/nixpkgs.nix;
-    };
-    nixci.default =
-      let
-        overrideInputs = {
-          rust-flake = ./.;
-        };
-      in
-      {
-        dev = { inherit overrideInputs; dir = "dev"; };
+    in
+    {
+      flakeModules = {
+        default = import' ./nix/modules/flake-module.nix { inherit rust-overlay crane; };
+        nixpkgs = ./nix/modules/nixpkgs.nix;
       };
-  };
+      nixci.default =
+        let
+          overrideInputs = {
+            rust-flake = ./.;
+          };
+        in
+        {
+          dev = { inherit overrideInputs; dir = "dev"; };
+        };
+    };
 }
