695281a76bc70754a66e92fabb3b0f09e538a41f
Nikhil Singh <nik.singh710@gmail.com>
2025-07-14 12:04:54 +0530

    chore: apply follow-up changes after merging globsetfix

--- examples/multi-crate/flake.nix
+++ examples/multi-crate/flake.nix
@@ -4,27 +4,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-flake.url = "github:juspay/rust-flake";
+    rust-flake.url = "path:../../."; # TODO: replace with url once <https://github.com/juspay/rust-flake/pull/40>
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      debug = true;
       imports = [
         inputs.rust-flake.flakeModules.default
         inputs.rust-flake.flakeModules.nixpkgs
       ];
 
-      perSystem = { self', ... }: {
+      perSystem = { self', config, ... }: {
         rust-project = {
           crateNixFile = "crate.nix";
-          crates = {
-            crate-a.path = (inputs.self) + /crates/crate-a;
-          };
         };
 
         devShells.default = self'.devShells.rust;
-        packages.default = self'.packages.crate-a;
+        packages.default = config.rust-project.crates.crate-a.crane.outputs.drv.crate;
       };
     };
 }
