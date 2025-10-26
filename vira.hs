057820efed0d959d177e2af7f5296c38f30f3ed8
Sridhar Ratnakumar <srid@srid.ca>
2025-10-25 20:37:36 -0400

    ci: Omnix -> vira

--- vira.hs
+++ vira.hs
@@ -2,6 +2,16 @@
 \ctx pipeline ->
   let isMain = ctx.branch == "main"
   in pipeline
-    { signoff.enable = True
+    { build.systems =
+        [ "x86_64-linux"
+        , "aarch64-darwin"
+        ]
+    , build.flakes =
+        [ "./dev" { overrideInputs = [("rust-flake", ".")] }
+        , "./nix/crate-parser/test" { overrideInputs = [("crate-parser", "path:./nix/crate-parser")] }
+        , "./examples/single-crate" { overrideInputs = [("rust-flake", ".")] }
+        , "./examples/multi-crate" { overrideInputs = [("rust-flake", ".")] }
+        ]
+    , signoff.enable = True
     , cache.url = if isMain then Just "https://cache.nixos.asia/oss" else Nothing
     }
